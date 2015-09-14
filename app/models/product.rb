f124b093e7a78491fa6ec647d784e6b9afba86e7
gandalf23 <gandalf23@rocketmail.com>
2015-09-14 16:41:01 +0000

    First commit

---
+++ app/models/product.rb
@@ -0,0 +1,8 @@
+class Product < ActiveRecord::Base
+    belongs_to :catagory
+    
+    has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>"}, :default_url => "/images/:style/missing.png"
+    
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+    
+end
